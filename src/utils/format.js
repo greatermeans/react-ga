6c3ed0f2abc800ea568f3c98938bdfd86665c0f7
greatermeans <george@philosophie.is>
2017-10-19 11:56:40 -0400

    remove redaction

--- src/utils/format.js
+++ src/utils/format.js
@@ -5,10 +5,10 @@ import warn from './console/warn';
 const redacted = 'REDACTED (Potential Email Address)';
 
 export default function format(s, titleCase) {
-  if (mightBeEmail(s)) {
-    warn('This arg looks like an email address, redacting.');
-    return redacted;
-  }
+//   if (mightBeEmail(s)) {
+//     warn('This arg looks like an email address, redacting.');
+//     return redacted;
+//   }
 
   if (titleCase) {
     return toTitleCase(s);
